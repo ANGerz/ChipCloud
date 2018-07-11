b436a1b6bc245bbdc2229a481c5feddff088485c
Antoine Gerard <antoine@smartnsoft.com>
2018-07-11 11:36:13 +0200

    Updated dependencies

--- demo/src/main/java/fisk/chipclouddemo/DemoActivity.java
+++ demo/src/main/java/fisk/chipclouddemo/DemoActivity.java
@@ -1,27 +1,28 @@
 package fisk.chipclouddemo;
 
 import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutCompat;
 import android.util.Log;
-import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 
 import com.google.android.flexbox.FlexboxLayout;
-
 import fisk.chipcloud.ChipCloud;
 import fisk.chipcloud.ChipCloudConfig;
 import fisk.chipcloud.ChipDeletedListener;
 import fisk.chipcloud.ChipListener;
 import fisk.chipclouddemo.demo.R;
 
-public class DemoActivity extends AppCompatActivity {
+public class DemoActivity
+    extends AppCompatActivity
+{
 
   private static final String TAG = "DemoActivity";
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
+  @Override
+  protected void onCreate(Bundle savedInstanceState)
+  {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_demo);
 
@@ -35,11 +36,11 @@ public class DemoActivity extends AppCompatActivity {
         .uncheckedTextColor(Color.parseColor("#000000"));
 
     ChipCloud drawableChipCloud = new ChipCloud(this, flexboxDrawable, drawableConfig);
-    drawableChipCloud.addChip("Anna A", ContextCompat.getDrawable(this, R.drawable.anna_a));
-    drawableChipCloud.addChip("Anna B", ContextCompat.getDrawable(this, R.drawable.anna_b));
-    drawableChipCloud.addChip("Anna C", ContextCompat.getDrawable(this, R.drawable.anna_c));
-    drawableChipCloud.addChip("Anna D", ContextCompat.getDrawable(this, R.drawable.anna_d));
-    drawableChipCloud.addChip("Anna E", ContextCompat.getDrawable(this, R.drawable.anna_e));
+    drawableChipCloud.addChip("Anna A", ContextCompat.getDrawable(this, R.drawable.anna_a), -1, false);
+    drawableChipCloud.addChip("Anna B", ContextCompat.getDrawable(this, R.drawable.anna_b), -1, false);
+    drawableChipCloud.addChip("Anna C", ContextCompat.getDrawable(this, R.drawable.anna_c), -1, false);
+    drawableChipCloud.addChip("Anna D", ContextCompat.getDrawable(this, R.drawable.anna_d), -1, false);
+    drawableChipCloud.addChip("Anna E", ContextCompat.getDrawable(this, R.drawable.anna_e), -1, false);
 
 
     FlexboxLayout flexboxDrawableWithClose = (FlexboxLayout) findViewById(R.id.flexbox_drawable_close);
@@ -53,11 +54,11 @@ public class DemoActivity extends AppCompatActivity {
         .showClose(Color.parseColor("#a6a6a6"), 500);
 
     ChipCloud drawableWithCloseChipCloud = new ChipCloud(this, flexboxDrawableWithClose, drawableWithCloseConfig);
-    drawableWithCloseChipCloud.addChip("Anna A", ContextCompat.getDrawable(this, R.drawable.anna_a));
-    drawableWithCloseChipCloud.addChip("Anna B", ContextCompat.getDrawable(this, R.drawable.anna_b));
-    drawableWithCloseChipCloud.addChip("Anna C", ContextCompat.getDrawable(this, R.drawable.anna_c));
-    drawableWithCloseChipCloud.addChip("Anna D", ContextCompat.getDrawable(this, R.drawable.anna_d));
-    drawableWithCloseChipCloud.addChip("Anna E", ContextCompat.getDrawable(this, R.drawable.anna_e));
+    drawableWithCloseChipCloud.addChip("Anna A", ContextCompat.getDrawable(this, R.drawable.anna_a), -1, false);
+    drawableWithCloseChipCloud.addChip("Anna B", ContextCompat.getDrawable(this, R.drawable.anna_b), -1, false);
+    drawableWithCloseChipCloud.addChip("Anna C", ContextCompat.getDrawable(this, R.drawable.anna_c), -1, false);
+    drawableWithCloseChipCloud.addChip("Anna D", ContextCompat.getDrawable(this, R.drawable.anna_d), -1, false);
+    drawableWithCloseChipCloud.addChip("Anna E", ContextCompat.getDrawable(this, R.drawable.anna_e), -1, false);
 
     FlexboxLayout flexbox = (FlexboxLayout) findViewById(R.id.flexbox);
 
@@ -80,10 +81,13 @@ public class DemoActivity extends AppCompatActivity {
     String label = chipCloud.getLabel(2);
     Log.d(TAG, "Label at index 2: " + label);
 
-    chipCloud.setListener(new ChipListener() {
+    chipCloud.setListener(new ChipListener()
+    {
       @Override
-      public void chipCheckedChange(int index, boolean checked, boolean userClick) {
-        if(userClick) {
+      public void chipCheckedChange(int index, boolean checked, boolean userClick)
+      {
+        if (userClick)
+        {
           Log.d(TAG, String.format("chipCheckedChange Label at index: %d checked: %s", index, checked));
         }
       }
@@ -112,9 +116,11 @@ public class DemoActivity extends AppCompatActivity {
     deleteableCloud.addChip("Hyena");
     deleteableCloud.addChip("Iguana");
 
-    deleteableCloud.setDeleteListener(new ChipDeletedListener() {
+    deleteableCloud.setDeleteListener(new ChipDeletedListener()
+    {
       @Override
-      public void chipDeleted(int index, String label) {
+      public void chipDeleted(int index, String label)
+      {
         Log.d(TAG, String.format("chipDeleted at index: %d label: %s", index, label));
       }
     });
